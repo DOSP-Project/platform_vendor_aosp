6629239e6b2f15d83237a01dee7507c1a37c1573
Ayush Dubey <ayushdubey70@gmail.com>
2017-08-16 18:21:28 +0530

    Initial DOSP

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,6 @@
 # Inherit common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/dosp/config/common.mk)
 
-$(call inherit-product, vendor/aosp/config/telephony.mk)
+$(call inherit-product, vendor/dosp/config/telephony.mk)
 
-$(call inherit-product, vendor/aosp/config/jdc_props.mk)
+$(call inherit-product, vendor/dosp/config/jdc_props.mk)
