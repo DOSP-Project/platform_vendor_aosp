7ab81e23c6f9c4b7344612cc3213686d7aec4419
Kukun Mishra <kukun7610@gmail.com>
2017-08-23 20:55:50 +0530

    update  LG-H850

--- products/desi_h850.mk
+++ products/desi_h850.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The JDCTeam
+# Copyright (C) 2016 The DOSP Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
